cb63f588ece4d67c3e6a4b2ffdc1e5ed4d6a40fe
joySarkar <joysarkar98600@gmail.com>
2026-02-23 22:37:38 +0600

    login working successfully

--- src/modules/Auth/auth.controller.ts
+++ src/modules/Auth/auth.controller.ts
@@ -16,6 +16,20 @@ const createUser = async (req: Request, res: Response) => {
     }
 }
 
+const loginUser = async (req: Request, res: Response) => {
+    try {
+        const result = await AuthService.loginUser(req.body)
+        res.status(200).json({
+            success: true,
+            message: "User create",
+            data: result
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export const AuthController = {
-    createUser
+    createUser,
+    loginUser
 };
--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,12 +1,54 @@
 import { prisma } from "../../lib/prisma";
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import { email } from "zod";
+
+export const secret  = "jfkjdjfdjfg"
 
 const createUser = async (payload: any) => {
+    const hashPassword = await bcrypt.hash(payload.password, 8);
+
+
     const result = await prisma.user.create({
-        data: payload
+        data: {
+            ...payload,
+            password: hashPassword
+        }
     })
-    return result;
+    const { password, ...newResult } = result
+    return newResult;
+}
+
+const loginUser = async (payload: any) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: payload.email
+        }
+    })
+
+    if(!user){
+        throw new Error("User not found!")
+    }
+
+    const userData = {
+        id: user.id,
+        name: user.name,
+        role: user.role,
+        status: user.status,
+        email: user.email
+    }
+
+    const token = jwt.sign(userData, secret, {expiresIn: "1d"})
+
+    return {
+        token,
+        user
+    }
+
+
 }
 
 export const AuthService = {
-    createUser
+    createUser,
+    loginUser
 };
